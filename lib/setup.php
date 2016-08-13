0dfc9f96ce17420cc2a3bf8ee5afd1ccb8683506
Kenny Rogers <kennyr87@gmail.com>
2016-08-13 18:54:34 -0400

    Change primary sidebar to contact-info

--- lib/setup.php
+++ lib/setup.php
@@ -84,8 +84,6 @@ function display_sidebar() {
     // The sidebar will NOT be displayed if ANY of the following return true.
     // @link https://codex.wordpress.org/Conditional_Tags
     is_404(),
-    is_front_page(),
-    is_page(),
   ]);
 
   return apply_filters('sage/display_sidebar', $display);

d64b03e05e00de7bae9db61e045f01841d9c3ec5
Kenny Rogers <kennyr87@gmail.com>
2016-10-01 19:55:23 -0400

    Add and style nav menu

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,22 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+  <nav class="navbar navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
+      <span class='ldc-header-name'>Lindsey Cameron</span></a><span class='ldc-header-school'>Stephen M. Ross School of Business</span>
+      </div>
+        <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu( array(
+            'theme_location'  => 'primary_navigation',
+            'container'       => 'div',
+            'depth'           => 2,
+            'container_class' => 'collapse navbar-collapse',
+            'menu_class'      => 'nav navbar-nav',
+            'walker'          => new wp_bootstrap_navwalker() )
+          );
+        endif;
+        ?>
   </div>
+</nav>
 </header>

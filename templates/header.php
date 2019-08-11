e1c443520d0b1b913f350d2d613c5c03eff3785a
Kenny Rogers <kennyr87@gmail.com>
2019-08-11 15:41:56 -0400

    Update header template
    - Remove Ross Bug
    - Add url to homepage
    - Update ldc-header-school
    - left align primary nav

--- templates/header.php
+++ templates/header.php
@@ -2,24 +2,32 @@
   <nav class="navbar navbar-default">
     <div class="container">
       <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#ldc-primary-nav" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href='<?php echo get_home_url(); ?>'>
       <span class="navbar-brand">
-        <img class='ldc-nav-brand' src='<?php echo get_template_directory_uri()?>/dist/images/ross_bug.png' >
         <span class='ldc-header-name'>Lindsey Cameron</span>
-        <span class='ldc-header-school'>Stephen M. Ross School of Business</span>
+        <span class='ldc-header-school hidden-xs'>Assistant Professor of Management</span>
       </span>
+      </a>
       </div>
-        <?php
-        if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu( array(
-            'theme_location'  => 'primary_navigation',
-            'container'       => 'div',
-            'depth'           => 2,
-            'container_class' => 'collapse navbar-collapse pull-right',
-            'menu_class'      => 'nav navbar-nav',
-            'walker'          => new wp_bootstrap_navwalker() )
-          );
-        endif;
-        ?>
-  </div>
-</nav>
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu( array(
+          'theme_location'  => 'primary_navigation',
+          'container'       => 'div',
+          'depth'           => 2,
+          'container_class' => 'collapse navbar-collapse pull-left',
+          'menu_class'      => 'nav navbar-nav',
+          'container_id'    => 'ldc-primary-nav',
+          'walker'          => new wp_bootstrap_navwalker() )
+        );
+      endif;
+      ?>
+    </div>
+  </nav>
 </header>

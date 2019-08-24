249289f19773a186dd68e80783be9631967048b4
Kenny Rogers <kennyr87@gmail.com>
2019-08-24 14:48:51 -0400

    Get fonts from google cdn

--- templates/head.php
+++ templates/head.php
@@ -2,5 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Domine&display=swap" rel="stylesheet">
   <?php wp_head(); ?>
 </head>

e6ffcc639f3cadfa1fdcea7845b1296798eb9b5c
Kenny Rogers <kennyr87@gmail.com>
2020-09-05 14:08:13 -0400

    Create footer and set min height

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,7 @@
-<footer class="content-info">
+<footer class="content-info footer">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+    <p class='footer__text'><i class="fa fa-copyright" aria-hidden="true"></i> 2020  &bullet; Lindsey D. Cameron 
+    &bullet; <a href='mailto:ldcamer@wharton.upenn.edu'>ldcamer@wharton.upenn.edu</a></p>
   </div>
 </footer>
